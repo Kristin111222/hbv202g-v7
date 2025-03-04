b8b3a2b84dea31759909500085b4259935e9a52e
kristin900 <kristineyjar@gmail.com>
2025-03-04 10:48:47 +0000

    maven

---
+++ ass4/src/main/java/is/hi/ksr11/hbv202g/ass4/App.java
@@ -0,0 +1,10 @@
+package is.hi.ksr11.hbv202g.ass4;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
